c633c112c6d6d79c83fce8e19eacbfc9ddb3b1ad
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-04-22 13:47:45 +0800

    幫我做一下create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,49 +1,53 @@
-create table champ(
-    champion_name varchar(15) not null,
-    champion_id int default 0 NOT NULL,
-    primary key (champion_id)
+create table atbats(
+    ab_id varchar(10) not null,
+    batter_id varchar(6) NOT NULL,
+    event varchar(30),
+    g_id varchar(9) not null,
+    inning int not null,
+    o int not null,
+    p_score int not null,
+    p_throws varchar(1) not null,
+    pitcher_id varchar(6) not null,
+    stand varchar(1) not null,
+    top boolean not null,
+    primary key (ab_id)
 );
-load data local infile './champs.csv'
-into table champ
+load data local infile './atbats.csv'
+into table atbats
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
-create table match_info(
-    match_id int default 0 NOT NULL,
-    duration int,
-    version varchar(15),
+create table ejections(
+    date  date NOT NULL,
+    BS varchar(1),
+    des varchar(300),
+    event_num int,
+    CORRECT varchar(1),
+    team carchar(3),
+    is_home_team boolean,
     primary key (match_id)
 );
-
-load data local infile './matches.csv'
-into table match_info
+load data local infile './ejections.csv'
+into table ejections
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
 
-create table participant(
-    player_id int NOT NULL,
-    match_id int NOT NULL,
-    player tinyint ,
-    champion_id int NOT NULL,
-    ss1 varchar(15),
-    ss2 varchar(15),
-    position varchar(13) NOT NULL,
-    primary key (player_id),
-    foreign key (match_id) references match_info(match_id)
+create table games(
+    
 );
-load data local infile './participants.csv'
+load data local infile './games.csv'
 into table participant
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
-create table teamban(
+create table pitches(
     match_id int NOT NULL,
     team char(1) NOT NULL,
     champion_id int NOT NULL,
@@ -51,14 +55,14 @@ create table teamban(
     primary key (match_id, banturn)
 );
 
-load data local infile './teambans.csv'
+load data local infile './pitches.csv'
 into table teamban
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
-create table stat(
+create table player_names(
     player_id int NOT NULL,
     win boolean,
     item1 smallint,
@@ -80,7 +84,7 @@ create table stat(
     firstblood boolean,
     primary key(player_id)
 );
-load data local infile './stats.csv'
+load data local infile './player_names.csv'
 into table stat
 fields terminated by ','
 enclosed by '"'
