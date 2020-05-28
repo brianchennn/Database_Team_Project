04cd0fffb3c6df3276239d4856514019573b335c
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-05-28 23:52:26 +0800

    batter_homerun_year.sql done

--- player_in_team.sql
+++ player_in_team.sql
@@ -1,5 +1,16 @@
-select substring(atbats.ab_id,1,4),player_names.first_name,player_names.last_name,games.home_team
+select substring(atbats.ab_id,1,4),player_names.first_name,player_names.last_name,games.home_team,games.g_id
 from atbats,games,player_names 
-where ((player_names.id = atbats.batter_id and atbats.top = "False") or (player_names.id = atbats.pitcher_id and atbats.top = "True") ) and games.g_id = atbats.g_id and player_names.first_name like "Masahiro"
+where (
+    (player_names.id = atbats.batter_id and atbats.top = "False") or (player_names.id = atbats.pitcher_id and atbats.top = "True") ) 
+    and games.g_id = atbats.g_id 
+    and player_names.first_name like "Aroldis"
+group by player_names.first_name,player_names.last_name,games.home_team
+order by substring(atbats.ab_id,1,4) , player_names.first_name,player_names.last_name asc
+
+
+
+select substring(atbats.ab_id,1,4),player_names.first_name,player_names.last_name,games.away_team,games.g_id
+from atbats,games,player_names 
+where ((player_names.id = atbats.batter_id and atbats.top = "True") or (player_names.id = atbats.pitcher_id and atbats.top = "False") ) and games.g_id = atbats.g_id and player_names.first_name like "Aroldis"
 group by player_names.first_name,player_names.last_name,games.home_team
 order by substring(atbats.ab_id,1,4) , player_names.first_name,player_names.last_name asc
