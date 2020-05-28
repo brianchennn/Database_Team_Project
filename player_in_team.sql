046f98e56d8d5f726e37faddd60e9a28b17e3661
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-05-28 23:29:43 +0800

    add player_in_team

---
+++ player_in_team.sql
@@ -0,0 +1,5 @@
+select substring(atbats.ab_id,1,4),player_names.first_name,player_names.last_name,games.home_team
+from atbats,games,player_names 
+where ((player_names.id = atbats.batter_id and atbats.top = "False") or (player_names.id = atbats.pitcher_id and atbats.top = "True") ) and games.g_id = atbats.g_id and player_names.first_name like "Masahiro"
+group by player_names.first_name,player_names.last_name,games.home_team
+order by substring(atbats.ab_id,1,4) , player_names.first_name,player_names.last_name asc
