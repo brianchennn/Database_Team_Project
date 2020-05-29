dfe79536c794edb941daec08995e4a2f9e2e98f5
HungDino <43983154+hungdino@users.noreply.github.com>
2020-05-29 20:52:28 +0800

    Create setup_GrafanaReader_in_mysql.sql

---
+++ setup_GrafanaReader_in_mysql.sql
@@ -0,0 +1,11 @@
+SHOW GLOBAL VARIABLES LIKE 'PORT';
+--show what port the mysql instance is running on
+
+CREATE USER 'GrafanaReader' IDENTIFIED BY 'grafana';
+GRANT SELECT ON MLB.atbats to 'GrafanaReader';
+GRANT SELECT ON MLB.ejections to 'GrafanaReader';
+GRANT SELECT ON MLB.games to 'GrafanaReader';
+GRANT SELECT ON MLB.pitches to 'GrafanaReader';
+GRANT SELECT ON MLB.player_names to 'GrafanaReader';
+
+
