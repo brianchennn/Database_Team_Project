e53f32599d674e1176af7fe896140dd0c2a0cb45
Lee <justwe8787@gmail.com>
2020-06-03 16:30:11 +0800

    update

--- LLRR.sql
+++ LLRR.sql
@@ -1,26 +1,42 @@
 /*球隊case 1*/
-/*左投剋左打 右投剋右打*/
-select cnt_baserun.year,cnt_baserun.dir as direction, cnt_baserun.cnt as cnt_baserun, cnt_atbat.cnt as cnt_atbat,(cnt_baserun.cnt)/(cnt_atbat.cnt) as SLG
+/*列出左投或右投對左打或右打的打擊率*/
+select cnt_baserun.year,
+	(case cnt_baserun.pitcher_batter when "LL" then "Left_Left" when "LR" then "Left_Right" when "RL" then "Right_Left" when "RR" then "Right_Right" END) as pitcher_batter_stand
+	, sum(cnt_baserun.cnt) as cnt_baserun, sum(cnt_atbat.cnt) as cnt_atbat,sum(cnt_baserun.cnt)/sum(cnt_atbat.cnt) as AVG
 from (
-	select substring(A.ab_id,1,4) as year, A.p_throws as dir, count(*) as cnt
+	select substring(A.ab_id,1,4) as year, A.batter_id, CONCAT_WS('',A.p_throws , A.stand) as pitcher_batter, count(*) as cnt
 	from atbats as A
 	where (A.event = "Single"
 		or A.event = "Double"
 		or A.event = "Triple"
 		or A.event = "Home Run%")
-	and A.p_throws = A.stand
-	group by substring(A.ab_id,1,4), A.p_throws) as cnt_baserun,
+	group by substring(A.ab_id,1,4), A.batter_id, CONCAT_WS('',A.p_throws , A.stand)) as cnt_baserun,
 	(
-	select substring(A.ab_id,1,4) as year, A.p_throws as dir, count(*) as cnt
+	select substring(A.ab_id,1,4) as year, A.batter_id, CONCAT_WS('',A.p_throws , A.stand) as pitcher_batter, count(*) as cnt
 	from atbats as A
-	where A.p_throws = A.stand
-		and A.event != "Walk" 
+	where (A.event != "Walk" 
 		and A.event != "Sac Fly" 
 		and A.event != "Sac Bunt" 
 		and A.event != "Hit By Pitch" 
 		and A.event != "Catcher Interference" 
-		and A.event != "Intent Walk"
-	group by substring(A.ab_id,1,4), A.p_throws) as cnt_atbat
+		and A.event != "Intent Walk")
+	group by substring(A.ab_id,1,4), A.batter_id, CONCAT_WS('',A.p_throws , A.stand)
+	having cnt >= 50) as cnt_atbat,
+	(
+	select substring(A.ab_id,1,4) as year, A.batter_id, count(*) as cnt
+	from atbats as A
+	where (A.event != "Walk" 
+		and A.event != "Sac Fly" 
+		and A.event != "Sac Bunt" 
+		and A.event != "Hit By Pitch" 
+		and A.event != "Catcher Interference" 
+		and A.event != "Intent Walk")
+	group by substring(A.ab_id,1,4), A.batter_id
+	having cnt >= 50) as cnt_player
 where cnt_baserun.year = cnt_atbat.year
-	and cnt_baserun.dir = cnt_atbat.dir;
+	and cnt_baserun.batter_id = cnt_atbat.batter_id
+	and cnt_baserun.batter_id = cnt_player.batter_id
+	and cnt_baserun.pitcher_batter = cnt_atbat.pitcher_batter
+group by cnt_baserun.year, cnt_baserun.pitcher_batter
+order by year, field(pitcher_batter_stand, "Left_Left", "Left_Right", "Right_Right", "Right_Left");
 	
--- first_inning_run_ratio.sql
+++ first_inning_run_ratio.sql
@@ -1,27 +1,24 @@
 /*球隊case 2*/
-/*查詢某球隊每一年的首局得分率, 首局總得分數*/
-/*以nya為模板*/
-select score.year, count(*) as total_game, count(if(score.p_score > 0, score.p_score, null))/count(*) as scoring_rate, sum(score.p_score) as total_score
+/*列出所有球隊每一年的首局得分率, 首局總得分數*/
+select score.year, score.team, count(*) as total_game, count(if(score.p_score > 0, score.p_score, null))/count(*) as scoring_rate, sum(score.p_score) as total_score
 from(
-	select G.home_team,A.p_score, A.g_id, home.year
+	select A.p_score, A.g_id, home.team, home.year
 	from(
-		select G.g_id, substring_index(G.date, "-", 1) as year
-		from games as G
-		)as home,
+		select G.g_id, G.home_team as team, substring_index(G.date, "-", 1) as year
+		from games as G)as home,
 		atbats as A
 	where home.g_id = A.g_id
 		and A.inning = 2
 		and A.top like "TRUE%"
-	group by A.p_score, A.g_id
+	group by A.p_score, A.g_id, home.team
 	union
-	select G.away_team,A.p_score, A.g_id, away.year
+	select A.p_score, A.g_id, away.team, away.year
 	from(
-		select G.g_id, substring_index(G.date, "-", 1) as year
-		from games as G
-		)as away,
+		select G.g_id, G.away_team as team, substring_index(G.date, "-", 1) as year
+		from games as G)as away,
 		atbats as A
 	where away.g_id = A.g_id
 		and A.inning = 1
 		and A.top like "FALSE%"
-	group by A.p_score, A.g_id) as score
-group by score.year;
+	group by A.p_score, A.g_id, away.team) as score
+group by score.year, score.team;
