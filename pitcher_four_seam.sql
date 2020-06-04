526a3de8634860e2dda0d784b5da0abbcda4c3f0
Lee <justwe8787@gmail.com>
2020-06-04 23:01:26 +0800

    update

---
+++ pitcher_four_seam.sql
@@ -0,0 +1,15 @@
+/*投手進階 case4*/
+/*每年度各球員四縫線快速球(FF)資訊*/
+select substring(A.ab_id,1,4) as year, N.first_name, N.last_name,
+	round(avg(f_cnt.start_speed),1) as ff_v0_avg, round(max(f_cnt.start_speed),1) as ff_v0_max,
+	round(avg(f_cnt.spin_rate),3) as ff_spin_rate_avg, round(max(f_cnt.spin_rate),3) as ff_spin_rate_max,
+	round(avg(f_cnt.velocity_delta),1) as ff_v_delta_avg, round(min(f_cnt.velocity_delta),1) as ff_v_delta_min
+from(
+	select P.ab_id, P.start_speed, P.spin_rate, (P.start_speed-P.end_speed) as velocity_delta
+	from pitches as P
+	where P.pitch_type = "FF") as f_cnt,
+	atbats as A,
+	player_names as N
+where A.ab_id = f_cnt.ab_id
+	and A.pitcher_id = N.id
+group by substring(A.ab_id,1,4), A.pitcher_id;
--- pitcher_two_seam.sql
+++ pitcher_two_seam.sql
@@ -1,5 +1,5 @@
 /*投手進階 case4*/
-/*每年度各球員快速球(fastball-FF)資訊*/
+/*每年度各球員二縫線快速球(FT)資訊*/
 select substring(A.ab_id,1,4) as year, N.first_name, N.last_name,
 	round(avg(f_cnt.start_speed),1) as ff_v0_avg, round(max(f_cnt.start_speed),1) as ff_v0_max,
 	round(avg(f_cnt.spin_rate),3) as ff_spin_rate_avg, round(max(f_cnt.spin_rate),3) as ff_spin_rate_max,
@@ -7,7 +7,7 @@ select substring(A.ab_id,1,4) as year, N.first_name, N.last_name,
 from(
 	select P.ab_id, P.start_speed, P.spin_rate, (P.start_speed-P.end_speed) as velocity_delta
 	from pitches as P
-	where P.pitch_type = "FF") as f_cnt,
+	where P.pitch_type = "FT") as f_cnt,
 	atbats as A,
 	player_names as N
 where A.ab_id = f_cnt.ab_id
