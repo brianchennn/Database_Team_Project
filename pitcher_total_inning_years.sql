592334432817251e7a5a46911724692ea2bb60f2
hungdino@gmail.com <hungdino@gmail.com>
2020-06-04 23:19:25 +0800

    updata pitcher_total_inning_years

--- pitcher_total_inning_years.sql
+++ pitcher_total_inning_years.sql
@@ -1,21 +1,21 @@
-create table total_innings_pitcher_years(
+create table pitcher_total_inning_years(
 SELECT 
     SUBSTRING(atbats2.ab_id, 1, 4) AS years,
     player_names.first_name,
     player_names.last_name,
-    COUNT(*) AS total_innings
+    count(o)/3 AS total_innings
 FROM
     player_names,
     (SELECT 
-        a.g_id, a.ab_id, a.pitcher_id
+        a.g_id, a.ab_id, a.pitcher_id, a.o
     FROM
         atbats AS a
-    WHERE
-        o = 3
+	where o != 0
 	) AS atbats2,
     games
 WHERE
     player_names.id = atbats2.pitcher_id
         AND atbats2.g_id = games.g_id
 GROUP BY years , player_names.first_name , player_names.last_name
-order by years,total_innings desc);
+order by total_innings asc
+);
