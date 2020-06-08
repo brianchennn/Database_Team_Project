53a40acd9478369191c92c234f63665b2968020c
Lee <justwe8787@gmail.com>
2020-06-08 17:58:08 +0800

    進階1到5完成, 待優化

---
+++ strike_out_ratio_for_every_pitch_type.sql.sql
@@ -0,0 +1,35 @@
+/*投手進階 case5*/
+/*所有的三振中、各球種使用比例*/
+
+/*temp table*/
+create table type_cnt(
+	select substring(P.ab_id,1,4) as year, A.pitcher_id, P.pitch_type, CONCAT_WS('_', P.b_count, P.s_count) as BS, count(P.pitch_type) as cnt
+	from(
+		select P.ab_id, A.pitcher_id, max(P.pitch_num) as n
+		from atbats as A, pitches as P
+		where (A.event = "Strikeout" or A.event = "Strikeout - DP")
+			and A.ab_id = P.ab_id
+		group by P.ab_id) as pitch_num,
+		pitches as P,
+		atbats as A
+	where P.ab_id = pitch_num.ab_id
+		and P.pitch_num = pitch_num.n
+		and A.ab_id = P.ab_id
+	group by substring(P.ab_id,1,4), A.pitcher_id, P.pitch_type, CONCAT_WS('_', P.b_count, P.s_count));
+	
+/*main query*/
+select T.year, N.id, N.first_name, N.last_name, T.BS as ball_strike, T.pitch_type, T.cnt/total.cnt as ratio
+from type_cnt as T,
+	(
+	select T.year, T.pitcher_id, T.BS, sum(T.cnt) as cnt
+	from type_cnt as T
+	group by T.year, T.pitcher_id, T.BS) as total,
+	player_names as N
+where T.pitcher_id = total.pitcher_id
+	and T.year = total.year
+	and T.pitcher_id = N.id
+	and T.BS = total.BS
+order by T.year, N.id, T.BS, T.pitch_type;
+
+/*drop the temp table*/
+drop table type_cnt;
--- strike_ratio_for_every_pitch_type.sql.sql
+++ strike_ratio_for_every_pitch_type.sql.sql
@@ -1,24 +1,22 @@
-/*投手進階 case2*/
-/*每年度, 2好球後各投手的球種使用比例, 次數及當下的好壞球數*/
-select pitch_type_cnt.year, N.id, N.first_name, N.last_name, pitch_type_cnt.BS as ball_strike, pitch_type_cnt.pitch_type, pitch_type_cnt.cnt/total.cnt as ratio
+/*投手進階 case3*/
+/*每年度, 各投手的球種使用比例, 次數及當下的好壞球數*/
+select total.year, N.id, N.first_name, N.last_name, total.BS as ball_strike, pitch_type_cnt.pitch_type, pitch_type_cnt.cnt/total.cnt as ratio
 from(
 	select substring(A.ab_id,1,4) as year, A.pitcher_id, ab_cnt.BS, sum(ab_cnt.cnt) as cnt
 	from(
 		select P.ab_id, CONCAT_WS('_', P.b_count, P.s_count) as BS, count(*) as cnt
 		from pitches as P
 		where P.pitch_type not like ""
-			and P.s_count = 2
 		group by P.ab_id, CONCAT_WS('_', P.b_count, P.s_count)) as ab_cnt,
 		atbats as A
 	where A.ab_id = ab_cnt.ab_id
 	group by substring(A.ab_id,1,4), A.pitcher_id, ab_cnt.BS) as total,
 	(
-		select substring(A.ab_id,1,4) as year, A.pitcher_id as pitcher_id, ab_type_cnt.pitch_type as pitch_type, ab_type_cnt.BS, sum(ab_type_cnt.cnt) as cnt
+		select substring(A.ab_id,1,4) as year, A.pitcher_id as pitcher_id, ab_type_cnt.BS, ab_type_cnt.pitch_type as pitch_type, sum(ab_type_cnt.cnt) as cnt
 		from(
 			select P.ab_id, P.pitch_type, CONCAT_WS('_', P.b_count, P.s_count) as BS, count(*) as cnt
 			from pitches as P
 			where P.pitch_type not like ""
-				and P.s_count = 2
 			group by P.ab_id, P.pitch_type, CONCAT_WS('_', P.b_count, P.s_count)) as ab_type_cnt,
 			atbats as A
 		where A.ab_id = ab_type_cnt.ab_id
@@ -26,6 +24,6 @@ from(
 	player_names as N
 where pitch_type_cnt.year = total.year
 	and pitch_type_cnt.pitcher_id = total.pitcher_id
-	and pitch_type_cnt.pitcher_id = N.id
 	and pitch_type_cnt.BS = total.BS
+	and pitch_type_cnt.pitcher_id = N.id
 order by total.year, N.id, total.BS, pitch_type_cnt.pitch_type;
