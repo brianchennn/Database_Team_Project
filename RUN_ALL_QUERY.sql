1628afce97df378424426571f9569b022d15b25d
HungDino <43983154+hungdino@users.noreply.github.com>
2020-06-09 22:54:47 +0800

    Update RUN_ALL_QUERY.sql

--- RUN_ALL_QUERY.sql
+++ RUN_ALL_QUERY.sql
@@ -3,12 +3,14 @@ SOURCE LLRR.sql;
 SOURCE RBI_year.sql;
 SOURCE batter_OBP.sql;
 SOURCE batter_avg.sql;
-SOURCE batter_create_table.sql;
 SOURCE batter_homerun.sql;
 SOURCE batter_ops.sql;
 SOURCE batter_slg.sql;
 SOURCE batter_strikeout.sql;
 SOURCE batter_walk.sql;
+
+SOURCE batter_create_table.sql;
+
 SOURCE ejection_info_player.sql;
 SOURCE ejection_info_team.sql;
 SOURCE ejection_max_player.sql;
