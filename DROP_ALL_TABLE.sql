5e48f77093a72e04f583ba233f565d881888a1bc
HungDino <43983154+hungdino@users.noreply.github.com>
2020-06-09 22:29:11 +0800

    Create DROP_ALL_TABLE.sql

---
+++ DROP_ALL_TABLE.sql
@@ -0,0 +1,26 @@
+USE MLB;
+DROP TABLE LLRR;
+DROP TABLE RBI_year;
+DROP TABLE batter_OBP;
+DROP TABLE batter_avg;
+DROP TABLE batter_create_table;
+DROP TABLE batter_homerun;
+DROP TABLE batter_ops;
+DROP TABLE batter_slg;
+DROP TABLE batter_strikeout;
+DROP TABLE batter_walk;
+DROP TABLE ejection_max_player;
+DROP TABLE ejection_team;
+DROP TABLE ff_Velocity_avg
+DROP TABLE ff_velocity_avg_years
+DROP TABLE ff_velocity_max;
+DROP TABLE ff_velocity_max_years;
+DROP TABLE first_inning_run_ratio;
+DROP TABLE payoff_pitch_ratio;
+DROP TABLE region_status;
+DROP TABLE spin_strike_rate;
+DROP TABLE type_cnt;
+DROP TABLE strike_ratio_for_every_pitch_type;
+DROP TABLE whip;
+DROP TABLE whip_years;
+
