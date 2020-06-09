cea0fe92a4fecd9dd5ac4a1c5a9bd34e4fbe9594
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-06-10 00:31:33 +0800

    report更新

--- batter_create_table.sql
+++ batter_create_table.sql
@@ -1,6 +1,6 @@
 create table batter_create_table(
-select batter_avg.year,batter_avg.id, batter_avg.first_name,batter_avg.last_name,batter_walk.PA as PA,batter_avg.atbat 
-		as AB, batter_slg._1B as "1B", batter_slg._2B as "2B", batter_slg._3B as "3B", batter_slg._HR as "HR",batter_obp.OBP, batter_ops.OPS, batter_strikeout.strikeout 
+select batter_avg.year,batter_avg.id, batter_avg.first_name,batter_avg.last_name,batter_walk.PA as PA,batter_avg.atbat, batter_avg.atbat 
+		as AB, batter_slg._H as "H", batter_slg._1B as "1B", batter_slg._2B as "2B", batter_slg._3B as "3B", batter_slg._HR as "HR",batter_obp.OBP, batter_ops.OPS, batter_strikeout.strikeout 
 		as K, batter_strikeout.strikeout_rate*9 as K9,
 		batter_walk.BB as BB, batter_walk.BB_rate*9 as BB9
 from batter_avg, batter_slg, batter_obp, batter_ops, batter_strikeout, batter_walk
