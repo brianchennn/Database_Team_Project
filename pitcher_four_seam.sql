aead1ca98f9d7b56cdd64ef2a9ded7ac751b70a0
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-06-10 01:44:25 +0800

    pitcher 重新命名

--- pitcher_four_seam.sql
+++ pitcher_four_seam.sql
@@ -1,9 +1,9 @@
 /*投手進階 case4*/
 /*每年度各球員四縫線快速球(FF)資訊*/
 select substring(A.ab_id,1,4) as year, N.id, N.first_name, N.last_name,
-	round(avg(f_cnt.start_speed),1) as ff_v0_avg, round(max(f_cnt.start_speed),1) as ff_v0_max,
-	round(avg(f_cnt.spin_rate),3) as ff_spin_rate_avg, round(max(f_cnt.spin_rate),3) as ff_spin_rate_max,
-	round(avg(f_cnt.velocity_delta),1) as ff_v_delta_avg, round(min(f_cnt.velocity_delta),1) as ff_v_delta_min
+	round(avg(f_cnt.start_speed),2) as ff_v0_avg, round(max(f_cnt.start_speed),2) as MAX_speed,
+	round(avg(f_cnt.spin_rate),0) as AVG_spin_rate, round(max(f_cnt.spin_rate),0) as MAX_spin_rate,
+	round(avg(f_cnt.velocity_delta),2) as AVG_speed_diff, round(min(f_cnt.velocity_delta),2) as MIN_speed_diff
 from(
 	select P.ab_id, P.start_speed, P.spin_rate, (P.start_speed-P.end_speed) as velocity_delta
 	from pitches as P
