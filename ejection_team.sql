ead6e9db260863bdf665ca7bc7a5c468f42ee509
Lee <justwe8787@gmail.com>
2020-06-09 21:55:43 +0800

    ejection_team.sql update

---
+++ ejection_team.sql
@@ -0,0 +1,9 @@
+/*球隊case 5*/
+/*每年度所有隊伍的ejection次數*/
+/*若某年某隊伍的ejection次數為0的話則不會顯示於表中*/
+select temp.year, temp.team, count(*) as cnt
+from (
+	select substring_index(E.date, "/", -1) as year, E.team
+	from ejections as E) as temp
+group by temp.year, temp.team;
+	
