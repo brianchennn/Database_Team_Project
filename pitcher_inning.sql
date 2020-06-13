7d3fd1c17a81ebd0122cd72bbcf6cdbc1f0a992d
Channing_Wang <a0911774228@gmail.com>
2020-06-13 15:41:55 +0800

    update pitcher_inning.sql

--- pitcher_inning.sql
+++ pitcher_inning.sql
@@ -11,7 +11,11 @@ FROM
     (SELECT 
         a.g_id, a.ab_id, a.pitcher_id,
         (case when a.event = 'Triple Play' then 3
-			  when a.event = 'Double Play' or a.event = 'Strikeout - DP' then 2
+			  when a.event = 'Double Play' or 
+				   a.event = 'Ground Into DP' or 
+				   a.event = 'Strikout - DP' or 
+				   a.event = 'Sac Fly DP' or 
+				   a.event = 'Sacrifice Bunt DP' then 2
               when a.event = 'Groundout' or
                    a.event = 'Strikeout' or
                    a.event = 'Runner Out' or
@@ -21,6 +25,10 @@ FROM
                    a.event = 'Lineout' or
                    a.event = 'Bunt Groundout' or
                    a.event = 'Bunt Pop Out' or
+                   a.event = 'Sac Bunt' or
+                   a.event = 'Sac Fly' or
+                   a.event = 'Fielders Choice Out' or
+                   a.event = 'Chatcher Interference' or
                    a.event = 'Bunt Lineout' then 1
 			  else 0 end) as Num_out
     FROM
