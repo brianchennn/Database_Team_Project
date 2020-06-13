ddbcd6d391219a577e236529a77aab6091ea2eed
Channing_Wang <a0911774228@gmail.com>
2020-06-13 12:16:23 +0800

    updata pitcher_inning.sql

--- pitcher_inning.sql
+++ pitcher_inning.sql
@@ -1,22 +1,33 @@
-create table pitcher_total_inning
+create table pitcher_inning
 (
 SELECT 
     SUBSTRING(atbats2.ab_id, 1, 4) AS years,
     player_names.id,
     player_names.first_name,
     player_names.last_name,
-    count(o)/3 AS total_inning
+    count(Num_out)/3 AS total_inning
 FROM
     player_names,
     (SELECT 
-        a.g_id, a.ab_id, a.pitcher_id, a.o
+        a.g_id, a.ab_id, a.pitcher_id,
+        (case when a.event = 'Triple Play' then 3
+			  when a.event = 'Double Play' or a.event = 'Strikeout - DP' then 2
+              when a.event = 'Groundout' or
+                   a.event = 'Strikeout' or
+                   a.event = 'Runner Out' or
+                   a.event = 'Flyout' or
+                   a.event = 'Forceout' or
+                   a.event = 'Pop Out' or
+                   a.event = 'Lineout' or
+                   a.event = 'Bunt Groundout' or
+                   a.event = 'Bunt Pop Out' or
+                   a.event = 'Bunt Lineout' then 1
+			  else 0 end) as Num_out
     FROM
         atbats AS a
 	where o != 0
-	) AS atbats2,
-    games
+	) AS atbats2
 WHERE
     player_names.id = atbats2.pitcher_id
-        AND atbats2.g_id = games.g_id
 GROUP BY years , player_names.id, player_names.first_name , player_names.last_name
 );
