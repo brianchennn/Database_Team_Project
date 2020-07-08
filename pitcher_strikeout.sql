6dca0149c9eab66a1d4b8e3c9a2d3882685eda78
Channing_Wang <a0911774228@gmail.com>
2020-07-08 22:05:56 +0800

    pitcher_strikeout.sql新增K9值

--- pitcher_strikeout.sql
+++ pitcher_strikeout.sql
@@ -1,21 +1,23 @@
+create pitcher_strikeout(
 SELECT 
-    SUBSTRING(Strikeout.ab_id, 1, 4) AS years,
+    Strikeout.years,
     player_names.id,
     player_names.first_name,
     player_names.last_name,
-    pitcher_total_inning.total_inning,
+    pitcher_inning.total_inning,
     COUNT(*) AS total_strikeout,
     count(*)/(total_inning*9) as K9
 FROM
     (SELECT 
-        a.ab_id, a.pitcher_id
+        SUBSTRING(a.ab_id, 1, 4) as years, a.pitcher_id
     FROM
         atbats AS a
     WHERE
         a.event = 'Strikeout'
 	) AS Strikeout,
     player_names,
-    pitcher_total_inning
+    pitcher_inning
 WHERE
-    player_names.id = Strikeout.pitcher_id and Strikeout.pitcher_id = pitcher_total_inning.id
-GROUP BY Strikeout.ab_id , player_names.id, player_names.first_name , player_names.last_name, pitcher_total_inning.total_inning;
+    Strikeout.years = pitcher_inning.years and player_names.id = Strikeout.pitcher_id and Strikeout.pitcher_id = pitcher_inning.id
+GROUP BY Strikeout.years , player_names.id, player_names.first_name , player_names.last_name, pitcher_inning.total_inning
+);
