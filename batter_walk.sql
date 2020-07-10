5f461f883ef64e65a6fb7af131ebf0b1d68ef7bc
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-10 13:51:45 +0800

    小修

--- batter_walk.sql
+++ batter_walk.sql
@@ -2,11 +2,11 @@ create table batter_walk(
 select Year,id,first_name,last_name,sum(T1.cnt_baserun) as BB,sum(T1.cnt_atbat) as PA,sum(T1.cnt_baserun)/sum(T1.cnt_atbat) as BB_rate
 from(
     select substring(ab_id,1,4) as Year,id,first_name,last_name,event,
-            if(event="Walk",count(*),0) as cnt_baserun, 
+            if(event like "%Walk%",count(*),0) as cnt_baserun, 
             count(*) as cnt_atbat
     from player_names,atbats 
     where player_names.id = atbats.batter_id 
-        and(event!="Intent Walk")
+        
         
     group by substring(ab_id,1,4) ,first_name,last_name,event ) as T1 
 group by Year,first_name,last_name
