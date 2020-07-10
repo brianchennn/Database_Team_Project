4f01616ee6e3c27fc5b134c307228651a6a07f03
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-10 13:57:44 +0800

    BB改成包IBB 加入IBB

--- batter_IBB.sql
+++ batter_IBB.sql
@@ -1,8 +1,8 @@
-create table batter_walk(
+create table batter_IBB(
 select Year,id,first_name,last_name,sum(T1.cnt_baserun) as BB,sum(T1.cnt_atbat) as PA,sum(T1.cnt_baserun)/sum(T1.cnt_atbat) as BB_rate
 from(
     select substring(ab_id,1,4) as Year,id,first_name,last_name,event,
-            if(event="Walk" or event="Intent Walk",count(*),0) as cnt_baserun, 
+            if(event="Intent Walk",count(*),0) as cnt_baserun, 
             count(*) as cnt_atbat
     from player_names,atbats 
     where player_names.id = atbats.batter_id 
