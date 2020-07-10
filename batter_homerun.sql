4315a0aee19467b7ff5201962d7cab35208b425a
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-10 13:26:13 +0800

    小修

--- batter_homerun.sql
+++ batter_homerun.sql
@@ -1,5 +1,6 @@
-create table batter_homerun(select substring(atbats.ab_id,1,4) as Year,id,first_name,last_name,event,count(*) as cnt 
+create table batter_homerun(
+select substring(atbats.ab_id,1,4) as Year,id,first_name,last_name,event,count(*) as cnt 
 from player_names,atbats 
 where player_names.id = atbats.batter_id and atbats.event="Home Run" 
-group by Year,first_name,last_name
+group by Year,id,first_name,last_name
 order by Year asc,cnt desc, first_name desc,last_name desc);
