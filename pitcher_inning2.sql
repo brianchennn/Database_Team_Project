50a7b75c8494cb459756b36adbdeb8bad321c63b
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-10 21:52:42 +0800

    投球局數inning總算完成 數字分毫不差

--- pitcher_inning2.sql
+++ pitcher_inning2.sql
@@ -1,15 +1,19 @@
-
-SELECT T1.rowid,T2.rowid,T1.g_id, T1.ab_id, T1.pitcher_id, T1.o
-FROM
-    (SELECT 
-        @rowid:=@rowid+1 as rowid ,a.g_id, a.ab_id, a.pitcher_id, a.o
-    FROM
-        atbats AS a,(SELECT @rowid := 0) as ini
-	) as T1,
-    (SELECT 
-        @rowid:=@rowid+1 as rowid ,a.g_id, a.ab_id, a.pitcher_id, a.o
-    FROM
-        atbats AS a,(SELECT @rowid := 0) as ini
-	) as T2
-WHERE T1.rowid=T2.rowid+1 and T1.pitcher_id=T2.pitcher_id
+create table pitcher_inning(
+SELECT years,
+       pl.id, 
+       pl.first_name, 
+       pl.last_name, 
+       sum(out_diff)/3 as IP
+FROM(
+    SELECT SUBSTRING(a.ab_id, 1, 4) as years, IF(@pre_out!=3 and @pre_g_id=g_id, a.o-@pre_out , a.o) as out_diff, 
+        @pre_out:=a.o, 
+        @pre_pitcher_id:=a.pitcher_id, 
+        @pre_g_id:=a.g_id, 
+        a.g_id, 
+        a.ab_id, a.pitcher_id, a.o
+    FROM    atbats AS a,(SELECT @pre_out := 0, @pre_pitcher_id="", @pre_g_id="") as ini) as T,
+            player_names as pl
+WHERE pl.id=T.pitcher_id
+GROUP BY years,pitcher_id);
+        
 
