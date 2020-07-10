c4194c08e7de19dfec8bda4c908762c3ba11c50d
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-10 21:58:34 +0800

    改成pitcher_inning.sql

--- pitcher_inning.sql
+++ pitcher_inning.sql
@@ -1,41 +1,19 @@
-create table pitcher_inning
-(
-SELECT 
-    SUBSTRING(atbats2.ab_id, 1, 4) AS years,
-    player_names.id,
-    player_names.first_name,
-    player_names.last_name,
-    sum(Num_out)/3 AS total_inning
-FROM
-    player_names,
-    (SELECT 
-        a.g_id, a.ab_id, a.pitcher_id,
-        (case when a.event = 'Triple Play' then 3
-			  when a.event = 'Double Play' or 
-				   a.event = 'Ground Into DP' or 
-				   a.event = 'Strikout - DP' or 
-				   a.event = 'Sac Fly DP' or 
-				   a.event = 'Sacrifice Bunt DP' then 2
-              when a.event = 'Groundout' or
-                   a.event = 'Strikeout' or
-                   a.event = 'Runner Out' or
-                   a.event = 'Flyout' or
-                   a.event = 'Forceout' or
-                   a.event = 'Pop Out' or
-                   a.event = 'Lineout' or
-                   a.event = 'Bunt Groundout' or
-                   a.event = 'Bunt Pop Out' or
-                   a.event = 'Sac Bunt' or
-                   a.event = 'Sac Fly' or
-                   a.event = 'Fielders Choice Out' or
-                   a.event = 'Chatcher Interference' or
-                   a.event = 'Bunt Lineout' then 1
-			  else 0 end) as Num_out
-    FROM
-        atbats AS a
-	where o != 0
-	) AS atbats2
-WHERE
-    player_names.id = atbats2.pitcher_id
-GROUP BY years , player_names.id, player_names.first_name , player_names.last_name
-);
+create table pitcher_inning(
+SELECT years,
+       pl.id, 
+       pl.first_name, 
+       pl.last_name, 
+       sum(out_diff)/3 as IP
+FROM(
+    SELECT SUBSTRING(a.ab_id, 1, 4) as years, IF(@pre_out!=3 and @pre_g_id=g_id, a.o-@pre_out , a.o) as out_diff, 
+        @pre_out:=a.o, 
+        @pre_pitcher_id:=a.pitcher_id, 
+        @pre_g_id:=a.g_id, 
+        a.g_id, 
+        a.ab_id, a.pitcher_id, a.o
+    FROM    atbats AS a,(SELECT @pre_out := 0, @pre_pitcher_id="", @pre_g_id="") as ini) as T,
+            player_names as pl
+WHERE pl.id=T.pitcher_id
+GROUP BY years,pitcher_id);
+        
+
