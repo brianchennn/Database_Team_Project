35f790ace1f598d55218dadef7ff2124e2623a90
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-11 16:42:00 +0800

    9,10整合在pitch_num_per_game.sql內

--- pitch_num_per_game.sql
+++ pitch_num_per_game.sql
@@ -1,4 +1,4 @@
-/*紀錄每年每場投手的用球數、局數*/
+/*紀錄每年每場投手的用球數、局數、平均每個出局的用囚涼*/
 create table temp(
     SELECT SUBSTRING(A.ab_id, 1, 4) AS years, A.g_id as g_id, P.ab_id, A.pitcher_id,P.pitch_num
     FROM pitches as P, atbats as A
@@ -6,7 +6,7 @@ create table temp(
 );
 drop table if exists pitch_num_per_game; 
 create table pitch_num_per_game(
-SELECT T1.years, T1.first_name, T1.last_name, T1.g_id, Pitch_per_Game , p_IP.IP
+SELECT T1.years, T1.first_name, T1.last_name, T1.g_id, Pitch_per_Game , p_IP.IP, Pitch_per_Game/IP/3 as pitch_per_out
 FROM(
     SELECT T.years, PL.first_name, PL.last_name,T.pitcher_id, T.g_id, count(pitch_num) as Pitch_per_Game
     FROM temp as T,  player_names as PL
@@ -14,6 +14,6 @@ FROM(
     GROUP BY years, PL.first_name, PL.last_name, T.g_id
     ) as T1, pitcher_inning as p_IP
 WHERE T1.g_id = p_IP.g_id and T1.pitcher_id = p_IP.id
-
+ORDER BY T1.years, Pitch_per_Game desc
 );
-drop if exists temp;
+drop table if exists temp;
