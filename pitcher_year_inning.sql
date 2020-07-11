358a093c96def4ad55d923a25bcce8be3b606d9f
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2020-07-11 10:43:29 +0800

    pitcher_inning.sql 分成pitcher_inning.sql跟pitcher_year_inning.sql

--- pitcher_year_inning.sql
+++ pitcher_year_inning.sql
@@ -1,11 +1,9 @@
-drop table pitcher_inning
-create table pitcher_inning(
+create table pitcher_year_inning(
 SELECT years,
-       g_id,
        pl.id, 
        pl.first_name, 
        pl.last_name, 
-       ROUND(sum(out_diff)/3,3) as IP
+       sum(out_diff)/3 as IP
 FROM(
     SELECT SUBSTRING(a.ab_id, 1, 4) as years, IF(@pre_out!=3 and @pre_g_id=g_id, a.o-@pre_out , a.o) as out_diff, 
         @pre_out:=a.o, 
@@ -16,6 +14,5 @@ FROM(
     FROM    atbats AS a,(SELECT @pre_out := 0, @pre_pitcher_id="", @pre_g_id="") as ini) as T,
             player_names as pl
 WHERE pl.id=T.pitcher_id
-GROUP BY years,g_id,pitcher_id );
+GROUP BY years,pitcher_id);
         
-
