b0a568a3ae5132513c7d245051ca532a34e0407d
Channing_Wang <a0911774228@gmail.com>
2020-07-12 22:59:02 +0800

    整合pitcher_changup.sql

--- pitcher_changeup.sql
+++ pitcher_changeup.sql
@@ -1,3 +1,26 @@
+/*先找出使用率超過5%的有誰*/
+DROP TABLE if EXISTS pitcher_changeup_use_more_than_5;
+CREATE table pitcher_changeup_use_more_than_5(
+    SELECT
+    SUBSTRING(a.ab_id, 1, 4) as years,
+    a.pitcher_id
+
+    FROM
+    (
+        SELECT p.ab_id, 
+               (case when p.pitch_type = "CH" then 1
+               else 0 end) as Use_count
+        from pitches as p
+    ) as P,
+    atbats as a
+
+    WHERE
+    P.ab_id = a.ab_id
+    GROUP BY years, a.pitcher_id
+    HAVING avg(Use_count) > 0.05
+);
+
+/*再分析該球種*/
 DROP TABLE if EXISTS pitcher_changeup;
 CREATE TABLE pitcher_changeup(
     SELECT
