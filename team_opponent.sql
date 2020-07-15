2a14b98de4b2c00e0d32dd2f8396c41cdb3c6aa1
HungDino <43983154+hungdino@users.noreply.github.com>
2020-07-15 14:23:06 +0800

    Update team_opponent.sql

--- team_opponent.sql
+++ team_opponent.sql
@@ -2,7 +2,7 @@
 /*查詢某球隊每場比賽的對手, 主隊或客隊, 比分*/
 /*以nya為模板*/
 drop table if exists team_opponent;
-create table opponent(
+create table team_opponent(
 select list.g_id, list.home_or_away, list.opponent, list.score, list.opponent_score
 from (
 	select ("home") as home_or_away, G.away_team as opponent, G.home_final_score as score, G.away_final_score as opponent_score, G.g_id as g_id
