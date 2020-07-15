aa625d737830f0baa3a9246c326ad080b185f01e
HungDino <43983154+hungdino@users.noreply.github.com>
2020-07-15 13:44:42 +0800

    Update ejection_info_player.sql

--- ejection_info_player.sql
+++ ejection_info_player.sql
@@ -8,3 +8,8 @@ where P.first_name like "Bryce%"
 	and P.last_name like "Harper%"
 	and P.id = E.player_id;
 	
+SELECT * FROM(
+select E.*, CONCAT(first_name, ' ', last_name) AS name
+from ejections as E, player_names as P
+where P.id = E.player_id) AS T
+WHERE name LIKE 'Bryce Harper';
